ec0ad68ada5d60da9e6096cda690ff44d2aae9c7
unknown <dienicosta@gmail.com>
2021-03-08 14:39:00 -0300

    suggestions styling

--- scripts/funtions.js
+++ scripts/funtions.js
@@ -1,50 +1,75 @@
 //SEARCH GIFOS FUNTION//
-async function getSearch (text, draw) {
-    let gifs = await fetch (`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${text}&limit=12`);
-    let gifsResult = await gifs.json();
-    console.log(gifsResult); 
-    if(!draw){
-    drawGifos(gifsResult); 
-    }
-    }
+async function getSearch(text, draw) {
+  let gifs = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${text}&limit=12`
+  );
+  let gifsResult = await gifs.json();
+  console.log(gifsResult);
+  if (!draw) {
+    drawGifos(gifsResult);
+  }
+}
 
 //DRAW GIFOS FUNTION//
-function drawGifos (gifsResult) {
-let resulGifos = document.getElementById('searcherReturnApi')
-//console.log(resulGifos)
-let array = gifsResult.data;
-array.forEach(elem => {                       
-    let card = document.createElement('div')
-    let draw = document.createElement('img');
+function drawGifos(gifsResult) {
+  let resulGifos = document.getElementById("searcherReturnApi");
+  //console.log(resulGifos)
+  let array = gifsResult.data;
+  array.forEach((elem) => {
+    let card = document.createElement("div");
+    let draw = document.createElement("img");
     draw.src = elem.images.original.url;
     card.classList.add("cardGift");
     card.appendChild(draw);
     resulGifos.appendChild(card);
-     
-});
-
+  });
 }
 
 // FUNCTION SUGGESTIONS //
 //let dt = await fetch(`https://api.giphy.com/v1/tags/related/${term}?api_key=${api_key}&limit=4`);
 
-async function suggestions (term) {
-    let suggest = await fetch (`https://api.giphy.com/v1/tags/related/${term}?api_key=${apiKey}&limit=4`);
-    let suggestResult = await suggest.json();
-    //console.log(suggestResult); 
-    drawSuggest (suggestResult);
-    
-} 
-    
+async function suggestions(term) {
+  let suggest = await fetch(
+    `https://api.giphy.com/v1/tags/related/${term}?api_key=${apiKey}&limit=4`
+  );
+  let suggestResult = await suggest.json();
+  //console.log(suggestResult);
+  drawSuggest(suggestResult);
+}
+// FUNCTION DRAWSUGGESTIONS //
 
-function drawSuggest (term) { 
-    console.log(term);
-     let suggestList = document.getElementById('suggestionsUl');   
-     let item = document.createElement("li"); 
-     let itemJson = term.data[0].name;
-     let itemSuggest = document.createTextNode(itemJson);   
-     
-     item.appendChild(itemSuggest);
-     suggestList.appendChild(item);         
+function drawSuggest(term) {
+  //console.log(term);  
+  let suggestList = document.getElementById("suggestionsUl");
+  suggestList.innerHTML= '';
+  let array = term.data;
+  array.forEach((element) => {
+    let item = document.createElement("li");
+    item.className += "sin_bullet";
+    let itemSuggest = document.createTextNode(element.name);
+    item.appendChild(itemSuggest);
+    
+    item.setAttribute("onclick", "select_list(this)");
+    suggestList.appendChild(item);
+    
+  });
 }
 
+function select_list(event) {
+  console.log("escucho");
+  console.log(event.innerText);
+  let resultado = event.innerText;
+  if (resultado) {
+    //Aca le pego a la api de search
+    getSearch(resultado);
+  }
+}
+
+
+// FUNCTION PRESUGGEST //
+// function presuggest (term) {
+// .addEventListener('keyup', () => {
+
+// });
+
+// }
--- scripts/gifos.js
+++ scripts/gifos.js
@@ -1,41 +1,47 @@
 // API INFO & PARAMETERS //
-const ApiBase = 'https://api.giphy.com/v1/gifs/';
-const apiKey = 'dvMF6ZEGC3KYgwMSAhpCKNiFt8nyxwwG'
+const ApiBase = "https://api.giphy.com/v1/gifs/";
+const apiKey = "dvMF6ZEGC3KYgwMSAhpCKNiFt8nyxwwG";
 
 // PROGRAMA //
 
 // TRENDING SECCION //
-let apiUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&q=s&limit=3`
-let ghipho = document.getElementById('trendingGifResults');
+let apiUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&q=s&limit=3`;
+let ghipho = document.getElementById("trendingGifResults");
 
 fetch(apiUrl)
-    .then(resp => resp.json())
-    .then(objGift => {
-        console.log(objGift)
-        let array = objGift.data;
-        //for ( let i = 0 ; i < array.length  ; i++ ) {
-        array.forEach(elem => {                       
-            let card = document.createElement('div')
-            let draw = document.createElement('img');
-            draw.src = elem.images.original.url;
-            card.classList.add("cardGift");
-            card.appendChild(draw);
-            ghipho.appendChild(card);
-        });
-
+  .then((resp) => resp.json())
+  .then((objGift) => {
+    console.log(objGift);
+    let array = objGift.data;
+    //for ( let i = 0 ; i < array.length  ; i++ ) {
+    array.forEach((elem) => {
+      let card = document.createElement("div");
+      let draw = document.createElement("img");
+      draw.src = elem.images.original.url;
+      card.classList.add("cardGift");
+      card.appendChild(draw);
+      ghipho.appendChild(card);
     });
-    
+  });
 
-    // SECCION SEARCH GIFOS //
-   
-    let text = document.getElementById('searchInput');
-    let btnTextInput = document.getElementById('searchBtn');
-    
-    btnTextInput.addEventListener('click',(e) => {
-    (e).preventDefault();    
+// SECCION SEARCH GIFOS //
+
+let text = document.getElementById("searchInput");
+let btnTextInput = document.getElementById("searchBtn");
+// text.addEventListener("keyup", () => {
+//   if (text.value.length > 2) {
+//     //document.getElementById("suggestionsBoxId").innerHTML = '';
+//     suggestions(text.value);
+//   }
+
+text.onkeyup = (e) => {
+  console.log(e.target.value);  
+  suggestions(text.value);
+};
+  btnTextInput.addEventListener("click", (e) => {
+    e.preventDefault();
     console.log(text.value);
-    getSearch(text.value);    
-    suggestions (text.value);
-    
-}); 
+    getSearch(text.value);
+  });
 
+  
